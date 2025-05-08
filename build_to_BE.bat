a90246f56155226be9c718e10f67de18516c76d6
beaconkiller <beaconkiller@gmail.com>
2025-05-08 14:34:06 +0700

    DEPLOY SEQUENCE - 2025-05-08

--- build_to_BE.bat
+++ build_to_BE.bat
@@ -6,22 +6,22 @@ for /f "tokens=2 delims==" %%I in ('"wmic os get localdatetime /value"') do set 
 set date=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2%
 
 
-@REM @REM ------------------------- GIT PUSH ON FLUTTER -------------------------
+@REM ------------------------- GIT PUSH ON FLUTTER -------------------------
 
-@REM cd /d "%src%"
-@REM git switch dev_branch_0.1 && ^
-@REM git add . && ^
-@REM git status && ^
-@REM git commit -m "DEPLOY SEQUENCE - %date%" && ^
-@REM git push origin dev_branch_0.1 && ^
+cd /d "%src%"
+git switch dev_branch_0.1 && ^
+git add . && ^
+git status && ^
+git commit -m "DEPLOY SEQUENCE - %date%" && ^
+git push origin dev_branch_0.1 && ^
 
 @REM ------------------------- GIT PUSH ON NODE BE -------------------------
 
 ng build && ^
 xcopy "%src%\dist\tf-e-approval\browser" "%src%\..\%be_folder%\web" /E /I /H /C /Y && ^
 cd /d "%src%\..\%be_folder%\" && ^
-git switch dev_branch_v0.2 && ^
+git switch dev_branch_0.1 && ^
 git add . && ^
 git status && ^
 git commit -m "DEPLOY SEQUENCE - %date%" && ^
-git push origin dev_branch_v0.2
+git push origin dev_branch_0.1
