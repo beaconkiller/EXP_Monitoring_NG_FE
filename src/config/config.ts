a404606ba6fd92019e57a3bcc0eab9e6085e915e
beaconkiller <beaconkiller@gmail.com>
2025-10-02 16:43:51 +0700

    DEPLOY SEQUENCE - ~0,4datetime:~4,2datetime:~6,2

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 export const config = {
     env_dev: {
-        host: "http://127.0.0.1:4098", // ======= > DEV
+        // host: "http://127.0.0.1:4098", // ======= > DEV
         // host: '' + '', // ============================ > PROD
-        // ws_host: '127.0.0.1' // HOST YOU DEPLOYED THIS BUILD INTO
+        ws_host: '127.0.0.1' // HOST YOU DEPLOYED THIS BUILD INTO
     }
 }
